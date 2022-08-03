7b455aa7b5653ee8e721be8277477caf0b17905c
Gary M <82660495+gary-ios@users.noreply.github.com>
2022-08-03 19:06:52 +0100

    Create Spring Boot project starter

---
+++ src/main/java/com/example/doiwebservice/DoiWebServiceApplication.java
@@ -0,0 +1,13 @@
+package com.example.doiwebservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoiWebServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoiWebServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/doiwebservice/DoiWebServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.doiwebservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoiWebServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
